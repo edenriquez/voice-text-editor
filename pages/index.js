1a852b7de3f6f7250482fd2d2383ac3b2436a3cb
Eduardo Enriquez <eduardo@MacBook-Air-de-Eduardo-E.local>
2018-04-02 13:25:04 -0500

    Script ot utils

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,30 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Head from 'next/head'
+import React, { Component } from 'react';
+import Speech from '../utils/speech'
+class App extends Component {
 
-export default class CustomDocument extends Document {
-    static async getInitialProps (ctx) {
-      return await Document.getInitialProps(ctx)
+    componentDidMount () {
+        const speechRec = new App();
+        // add service worker
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker
+                    .register('./service-worker.js')
+                    .then(function() { console.log('Service Worker Registered'); });
+        }
     }
-render () {
-    return (
-        <html>
-        <Head>
-          <title>Speech Recog</title>
-          <link href="/static/stylesheets/style.css" rel="stylesheet" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-          <script src="/lib/main.js"></script>
-        </body>
-      </html>
-    )
-  }
+    render(){
+    return(
+        <div>
+            <Head>
+            <link rel='stylesheet' href='/static/stylesheets/style.css' />
+            </Head>
+            <body>
+                <div>
+                    <h1>Speech Recognition</h1>
+                </div>
+            </body>
+        </div>
+        )
+    }
 }
+export default App;
